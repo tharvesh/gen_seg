4060c1e99996dd9ddd22492a0e6078dcac68a7d0
tharvesh <tharveshliyakat@gmail.com>
2015-06-29 16:44:00 +0200

    working bedmap function and writing format output function

--- src/gen_seg.py
+++ src/gen_seg.py
@@ -6,6 +6,7 @@ from optparse import OptionParser
 import subprocess
 import os
 import os.path
+from itertools import izip
 
 
 # Method to parse options
@@ -17,18 +18,27 @@ def parse_option():
     parser.add_option("-w", "--window-size", dest="win_size", help="Size of the window to use", metavar="INT")
     parser.add_option("-s", "--step-size", dest="step_size", help="Base pairs to step before creating a new window",
                       default=None, metavar="INT")
+    parser.add_option("-b", "--bedmap", dest="bedmap_path", help="Path to bedmap bin", default=None, metavar="FILE")
+    parser.add_option("-o", "--outfile", dest="out_base", help="Basename of the output file", default=None,
+                      metavar="STRING")
     (options, args) = parser.parse_args()
 
     bam_folder = options.folder
     g_idx = options.g_idx
     win_size = options.win_size
+    bedmap_path = options.bedmap_path
+    out_base = options.out_base
+
+    if bedmap_path == None:
+        print "Path to bedmap is missing"
+        exit()
 
     if options.step_size != None:
         step_size = options.step_size
     else:
         step_size = win_size
 
-    return (bam_folder, g_idx, win_size, step_size)
+    return (bam_folder, g_idx, win_size, step_size, bedmap_path, out_base)
 
 
 # Method to make bed file with given window and step sizes
@@ -51,19 +61,23 @@ def run_make_windows(g_idx, win_size, step_size):
 
 def run_bamtobed(bam_folder):
     # Making bed folder
-    dir_to_make = os.getcwd() + "/bed"
-    if not os.path.exists(dir_to_make):
-        os.makedirs(dir_to_make)
-    else:
-        pass
-    bed_dir = dir_to_make
+    """
+    Returns path to directory of the bed
+    :rtype : str
+    """
 
     if os.path.isdir(bam_folder):
+        dir_to_make = os.getcwd() + "/bed"
+        if not os.path.exists(dir_to_make):
+            os.makedirs(dir_to_make)
+        else:
+            pass
+        bed_dir = dir_to_make
         for fl in os.listdir(bam_folder):
             if fl.endswith('.bam'):
                 # Creating output filename
                 outflname = os.path.splitext(fl)[0] + '.bed'
-                outflname = os.path.join(bed_dir,outflname)
+                outflname = os.path.join(bed_dir, outflname)
                 # Creata an instance of file with write mode
                 out = open(outflname, "w")
                 fl = os.path.join(bam_folder, fl)
@@ -72,23 +86,55 @@ def run_bamtobed(bam_folder):
 
                 for line in p.stdout:
                     out.write("%s" % line)
+                out.close()
         return bed_dir
     else:
         print 'Unable find the given bam directory'
         exit()
 
 
-def run_bedmap():
-    pass
+def run_bedmap(bedmap_path, bed_dir, created_windows_bed_fname):
+    counts = []
+    created_windows_bed_fname = os.getcwd() + "/" + created_windows_bed_fname
+    for bed in os.listdir(bed_dir):
+        bed = bed_dir + "/" + bed
+        cmd = [bedmap_path, "--fraction-map", "0.5", "--count", created_windows_bed_fname, bed]
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+
+        temp = []
+        for item in p.stdout:
+            temp.append(item.strip())
+        counts.append(temp)
+    return counts
+
+
+def format_output(counts, out_base, created_w_bed_fname):
+    if out_base == None:
+        for l1, l2, l3 in izip(*counts):
+            print str(l1) + '\t' + str(l2) + '\t' + str(l3)
+    else:
+        created_w_bed_fname = os.getcwd() + "/" + created_w_bed_fname
+
+        # Chunk to write first column of the text file
+        first_col = []
+        for line in open(created_w_bed_fname).readlines():
+            line = line.strip()
+            line = line.replace("\t","_")
+            first_col.append(line)
+
+        out_mat = out_base + ".matrix"
+        out_txt = out_base + ".txt"
+        out1 = open(out_mat,"w")
 
 
 def main():
-    (bam_folder, g_idx, win_size, step_size) = parse_option()
+    (bam_folder, g_idx, win_size, step_size, bedmap_path, out_base) = parse_option()
 
     # print bam_folder, g_idx,win_size,step_size
-    windows_bed = run_make_windows(g_idx, win_size, step_size)
+    created_windows_bed_fname = run_make_windows(g_idx, win_size, step_size)
     bed_dir = run_bamtobed(bam_folder)
-    
+    counts = run_bedmap(bedmap_path, bed_dir, created_windows_bed_fname)
+    format_output(counts, out_base, created_windows_bed_fname)
 
 
 if __name__ == "__main__":
