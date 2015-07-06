46de59177e2690a36355908b4481d2203e2205b9
tharvesh <tharveshliyakat@gmail.com>
2015-07-06 16:34:27 +0200

    RProject started

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='gen_seg',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='tharvesh',
+    author_email='',
+    description=''
+)
